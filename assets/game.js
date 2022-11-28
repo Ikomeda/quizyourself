e7d79a09bec5432079b7ff4fe59c02f8aed9eb05
Payton <rekrapnotyap@gmail.com>
2022-11-27 21:23:10 -0700

    added a timer to the quiz

--- assets/game.js
+++ assets/game.js
@@ -3,12 +3,14 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+const timerText = document.querySelector('#timer');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let timer = 40;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [
     {
@@ -53,6 +55,21 @@ startGame = () => {
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
+    setTime()
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timer--;
+        timerText.textContent = timer;
+
+        if(timer === 0) {
+            clearInterval(timerInterval);
+
+            localStorage.setItem("mostRecentScore", score);
+            return window.location.assign('/end.html')
+        }
+    }, 1000);
 }
 
 getNewQuestion = () => {
